17325d450b7bf7f9140b23a1db88040864cdb816
rkdtnlzl <rkdtnlzl@naver.com>
2024-06-21 21:34:46 +0900

    [Feat] 이미지뷰 로드함수 Extension

---
+++ WeatherApp/Extension/UIImageView+.swift
@@ -0,0 +1,22 @@
+//
+//  UIImageView+.swift
+//  WeatherApp
+//
+//  Created by 강석호 on 6/21/24.
+//
+
+import UIKit
+
+extension UIImageView {
+    func load(url: URL) {
+        DispatchQueue.global().async { [weak self] in
+            if let data = try? Data(contentsOf: url) {
+                if let image = UIImage(data: data) {
+                    DispatchQueue.main.async {
+                        self?.image = image
+                    }
+                }
+            }
+        }
+    }
+}
