39f63330fb324363bf890211c664e4393c9f2c63
rkdtnlzl <rkdtnlzl@naver.com>
2024-06-21 21:36:03 +0900

    [Fix] 루트뷰 수정

--- WeatherApp/SceneDelegate.swift
+++ WeatherApp/SceneDelegate.swift
@@ -14,7 +14,7 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
     
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
         guard let windowScene = (scene as? UIWindowScene) else { return }
-        let mainVC = ViewController()
+        let mainVC = WeatherViewController()
         self.window = UIWindow(windowScene: windowScene)
         self.window?.rootViewController = mainVC
         self.window?.makeKeyAndVisible()
